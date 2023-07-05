ecb42900d18b8b24bab0a82317678a1c80ef391b
Miguel Garza Carranza <maiky@meta.com>
2023-07-05 11:20:47 -0700

    Added Login navbar

--- ans2ask-ui/src/components/Login/Login.jsx
+++ ans2ask-ui/src/components/Login/Login.jsx
@@ -4,7 +4,14 @@ import "./Login.css";
 export default function Login() {
     return (
       <div className="login">
-        login
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container">
+                <div className="d-flex justify-content-between align-items-center w-100">
+                <a className="navbar-brand" href="#">Ans2Ask</a>
+                <button type="button" class="btn btn-outline-dark">Login</button>
+                </div>
+            </div>
+        </nav>
       </div>
     );
   }
