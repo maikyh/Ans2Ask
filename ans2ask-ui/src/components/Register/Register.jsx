6b061960189af4128b58c86cb981315753dbe66f
Miguel Garza Carranza <maiky@meta.com>
2023-07-05 13:09:31 -0700

    Minor changes

--- ans2ask-ui/src/components/Register/Register.jsx
+++ ans2ask-ui/src/components/Register/Register.jsx
@@ -7,8 +7,8 @@ export default function Register() {
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container">
                 <div className="d-flex justify-content-between align-items-center w-100">
-                <a className="navbar-brand" href="#">Ans2Ask</a>
-                <button type="button" class="btn btn-outline-dark">Login</button>
+                    <a className="navbar-brand" href="#">Ans2Ask</a>
+                    <button type="button" class="btn btn-outline-dark">Login</button>
                 </div>
             </div>
         </nav>
@@ -42,7 +42,7 @@ export default function Register() {
         <footer className="bg-light py-4">
             <div className="container text-center">
                 <p className="text-muted mb-0">
-                &copy; {new Date().getFullYear()} Ans2Ask. All rights reserved.
+                    &copy; {new Date().getFullYear()} Ans2Ask. All rights reserved.
                 </p>
             </div>
         </footer>
