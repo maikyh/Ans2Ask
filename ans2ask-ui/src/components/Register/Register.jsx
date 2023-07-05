6fcda381dc95d90953aa1b9331e1bfd2f22c707f
Miguel Garza Carranza <maiky@meta.com>
2023-07-05 00:00:12 -0700

    Created the form to register users

--- ans2ask-ui/src/components/Register/Register.jsx
+++ ans2ask-ui/src/components/Register/Register.jsx
@@ -13,6 +13,32 @@ export default function Register() {
             </div>
         </nav>
 
+        <div className="d-flex justify-content-center align-items-center custom-margin">
+            <div className="custom-container bg-light p-4 border rounded px-5">
+                <h1 className="text-center mb-4 fw-bold">Welcome to Ans2Ask</h1>
+                <form>
+                <div className="form-group mb-4">
+                    <label className="mb-2 fw-bold" htmlFor="username">Username</label>
+                    <input type="text" className="form-control bg-lighter" id="username" placeholder="" />
+                </div>
+                <div className="form-group mb-4">
+                    <label className="mb-2 fw-bold" htmlFor="email">Email</label>
+                    <input type="email" className="form-control bg-lighter" id="email" placeholder="" />
+                </div>
+                <div className="form-group mb-4">
+                    <label className="mb-2 fw-bold" htmlFor="password">Password</label>
+                    <input type="password" className="form-control bg-lighter" id="password" placeholder="" />
+                </div>
+                <div className="text-center">
+                    <div className="mb-2">
+                        <a className="custom-link" href="#" >Already have an account?</a>
+                    </div>
+                    <button type="submit" className="btn btn-dark w-100 d-block fw-bold">Register</button>
+                </div>
+                </form>
+            </div>
+        </div>
+
         <footer className="bg-light py-4">
             <div className="container text-center">
                 <p className="text-muted mb-0">
