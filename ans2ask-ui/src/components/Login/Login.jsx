b8a16cf10bdfda13d203a006394da215bd034450
Miguel Garza Carranza <maiky@meta.com>
2023-07-05 11:37:56 -0700

    Created the login form

--- ans2ask-ui/src/components/Login/Login.jsx
+++ ans2ask-ui/src/components/Login/Login.jsx
@@ -8,10 +8,32 @@ export default function Login() {
             <div className="container">
                 <div className="d-flex justify-content-between align-items-center w-100">
                 <a className="navbar-brand" href="#">Ans2Ask</a>
-                <button type="button" class="btn btn-outline-dark">Login</button>
+                <button type="button" class="btn btn-outline-dark">Register</button>
                 </div>
             </div>
         </nav>
+
+        <div className="d-flex justify-content-center align-items-center custom-margin">
+            <div className="custom-container bg-light p-4 border rounded px-5">
+                <h1 className="text-center mb-4 fw-bold">Ans2Ask</h1>
+                <form>
+                <div className="form-group mb-4">
+                    <label className="mb-2 fw-bold" htmlFor="usernameOrEmail">Username or Email</label>
+                    <input type="text" className="form-control bg-lighter" id="usernameOrEmail" placeholder="" />
+                </div>
+                <div className="form-group mb-4">
+                    <label className="mb-2 fw-bold" htmlFor="password">Password</label>
+                    <input type="password" className="form-control bg-lighter" id="password" placeholder="" />
+                </div>
+                <div className="text-center">
+                    <div className="mb-2">
+                        <a className="custom-link" href="#" >Don't have an account?</a>
+                    </div>
+                    <button type="submit" className="btn btn-dark w-100 d-block fw-bold">Login</button>
+                </div>
+                </form>
+            </div>
+        </div>
 
         <footer className="bg-light py-4">
             <div className="container text-center">
