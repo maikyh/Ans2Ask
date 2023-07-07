c82c9d3f293398d2e1c35ff9781d1e3c08022b66
Miguel Garza Carranza <maiky@meta.com>
2023-07-07 15:52:46 -0700

    Migrated the subjetcs code to its own component Subjetcs.jsx

--- ans2ask-ui/src/components/Home/Home.jsx
+++ ans2ask-ui/src/components/Home/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
 import QuestionGrid from "../QuestionGrid/QuestionGrid";
 import QuestionsOrCourses from "../QuestionsOrCourses/QuestionsOrCourses";
+import Subjects from "../Subjects/Subjects";
 import "./Home.css";
 
 export default function Home() {
@@ -11,10 +12,7 @@ export default function Home() {
         <Navbar/>
         <div className="d-flex justify-content-center align-items-center">
             <div className="custom-container-home bg-light px-4 pt-4 pb-2">
-                <div className="subjects-section bg-dark mb-4"></div>
-
-                <h1 className="text-center mb-2 fw-bold">Subject</h1>
-                
+                <Subjects/>
                 <QuestionsOrCourses/>
                 <QuestionGrid/>
             </div>
---
+++ ans2ask-ui/src/components/Subjects/Subjects.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Subjects.css";
+
+export default function Subjects() {
+  return (
+    <div className="subjects">
+      <div className="subjects-section bg-dark mb-4"></div>
+      <h1 className="text-center mb-2 fw-bold">Subject</h1>
+    </div>
+  );
+}
