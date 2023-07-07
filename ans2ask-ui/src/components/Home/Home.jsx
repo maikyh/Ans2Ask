38c509f6f6aa3333d827a909a02a0a9dccaaf27e
Miguel Garza Carranza <maiky@meta.com>
2023-07-07 12:14:56 -0700

    Finished the format of each question

--- ans2ask-ui/src/components/Home/Home.jsx
+++ ans2ask-ui/src/components/Home/Home.jsx
@@ -9,21 +9,21 @@ export default function Home() {
     <div className="home">
         <Navbar/>
         <div className="d-flex justify-content-center align-items-center custom-margin-home">
-            <div className="custom-container-home bg-light p-4">
+            <div className="custom-container-home bg-light px-4 pt-4 pb-2">
                 <div className="subjects-section bg-dark mb-4"></div>
 
                 <h1 className="text-center mb-2 fw-bold">Subject</h1>
                 
                 <div className="d-flex">
-                  <button className="btn btn-dark w-100 d-block fw-bold m-4 p-3">Questions</button>
-                  <button className="btn btn-dark w-100 d-block fw-bold m-4 p-3">Courses</button>
+                  <button className="btn btn-dark w-100 d-block fw-bold mx-4 mt-4 mb-1 p-3">Questions</button>
+                  <button className="btn btn-dark w-100 d-block fw-bold mx-4 mt-4 mb-1 p-3">Courses</button>
                 </div>
                 
                 <div className="">
-                    <Question id={1} username={"Miguel"} />
-                    <Question id={2} username={"Diego"} />
+                    <Question id={1} username={"Miguel"} subject={"Subject"} />
+                    <Question id={2} username={"Diego"} subject={"Subject"} />
+                    <Question id={3} username={"Finn"} subject={"Subject"} />
                 </div>
-              
             </div>
         </div>
         <Footer/>
--- ans2ask-ui/src/components/Question/Question.jsx
+++ ans2ask-ui/src/components/Question/Question.jsx
@@ -1,28 +1,35 @@
 import React from "react";  
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBell, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 import "./Question.css";
 
 export default function Question({id, username, subject}) {
   return (
     <div className="question">
-      <div className="question-card bg-white mb-4 p-3">
+      <div className="question-card bg-white mt-4 p-3">
         <div className="row">
           <div className="col-auto">
             <FontAwesomeIcon icon={faUser} />
           </div>
+
           <div className="col-auto">
             <h6 className="mt-1"> {username} </h6>
           </div>
-          <div>
-            <h6 className="" ></h6>
+
+          <div className="col-auto"> <h6 className="mt-1"> - </h6> </div>
+
+          <div className="col-auto">
+            <h6 className="mt-1"> {subject} </h6>
           </div>
           <div>
 
           </div>
         </div>
-        <div className="col-auto">
-          <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio, nulla sed perspiciatis eligendi quisquam similique temporibus eos recusandae illo earum, soluta optio quas. Similique, non! Dolorum, laudantium? Distinctio veritatis aliquam sequi voluptates odio eveniet nisi, similique fuga culpa ex assumenda cupiditate id, deleniti modi vitae corporis sapiente? Eum dolores doloremque pariatur ratione, distinctio modi et nostrum vitae eveniet qui dolore? </p>
+        <div>
+          <span className="fw-bold">Lorem ipsum dolor sit amet consectetur.</span>
+        </div>
+        <div className="">
+          <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim ducimus, blanditiis veniam tempore, animi porro perferendis eaque sequi laboriosam, illo ad adipisci praesentium eveniet temporibus suscipit? Fuga velit est non corrupti odio neque, molestias veritatis. Esse dicta nemo quis velit? Ex veritatis voluptates eaque corrupti. </p>
         </div>
       </div>
     </div>
