97ba21df3b28694552ba0628828b63ccc8a191cc
Miguel Garza Carranza <maiky@meta.com>
2023-07-07 15:44:20 -0700

    Migrated the questions or courses code section to its own component QuestionsOrCourses.jsx

--- ans2ask-ui/src/components/Home/Home.jsx
+++ ans2ask-ui/src/components/Home/Home.jsx
@@ -2,23 +2,20 @@ import React from "react";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
 import QuestionGrid from "../QuestionGrid/QuestionGrid";
+import QuestionsOrCourses from "../QuestionsOrCourses/QuestionsOrCourses";
 import "./Home.css";
 
 export default function Home() {
   return (
     <div className="home">
         <Navbar/>
-        <div className="d-flex justify-content-center align-items-center custom-margin-home">
+        <div className="d-flex justify-content-center align-items-center">
             <div className="custom-container-home bg-light px-4 pt-4 pb-2">
                 <div className="subjects-section bg-dark mb-4"></div>
 
                 <h1 className="text-center mb-2 fw-bold">Subject</h1>
                 
-                <div className="d-flex">
-                  <button className="btn btn-dark w-100 d-block fw-bold mx-4 mt-4 mb-1 p-2"> <h4>Questions</h4> </button>
-                  <button className="btn btn-dark w-100 d-block fw-bold mx-4 mt-4 mb-1 p-2"> <h4>Courses</h4> </button>
-                </div>
-                
+                <QuestionsOrCourses/>
                 <QuestionGrid/>
             </div>
         </div>
---
+++ ans2ask-ui/src/components/QuestionsOrCourses/QuestionsOrCourses.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./QuestionsOrCourses.css";
+
+export default function QuestionsOrCourses() {
+  return (
+    <div className="QuestionsOrCourses">
+        <div className="d-flex">
+            <button className="btn btn-dark w-100 d-block fw-bold mx-4 mt-4 mb-1 p-2"> <h4>Questions</h4> </button>
+            <button className="btn btn-dark w-100 d-block fw-bold mx-4 mt-4 mb-1 p-2"> <h4>Courses</h4> </button>
+        </div>
+    </div>
+  );
+}
