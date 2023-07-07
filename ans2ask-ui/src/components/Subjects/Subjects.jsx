91d2e3677153ef896e800081673de773c9a9f1ad
Miguel Garza Carranza <maiky@meta.com>
2023-07-07 16:39:29 -0700

    Added icons in subjects section

--- ans2ask-ui/src/components/Subjects/Subjects.jsx
+++ ans2ask-ui/src/components/Subjects/Subjects.jsx
@@ -1,14 +1,52 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSquareRootAlt, faCode, faGlobe, faMicroscope, faHeartbeat, faPalette, faChartLine, faBalanceScale, faDollarSign } from "@fortawesome/free-solid-svg-icons";
+import { faBrain, faSquareRootAlt, faCode, faGlobe, faMicroscope, faHeartbeat, faPalette, faChartLine, faBalanceScale, faDollarSign } from "@fortawesome/free-solid-svg-icons";
 import "./Subjects.css";
 
 export default function Subjects() {
   return (
     <div className="subjects">
-      <div className="subjects-section bg-dark mb-4"></div>
-      <h1 className="text-center mb-2 fw-bold">Subject</h1>
-      <div className="row">
+      <div className="row justify-content-center mt-2">
+      <div className="col-auto d-flex flex-column align-items-center">
+          <FontAwesomeIcon icon={faBrain} />
+          <p className="text-center">All</p>
+        </div>
+        <div className="col-auto d-flex flex-column align-items-center">
+          <FontAwesomeIcon icon={faCode} />
+          <p className="text-center">Informatics</p>
+        </div>
+        <div className="col-auto d-flex flex-column align-items-center">
+          <FontAwesomeIcon icon={faSquareRootAlt} />
+          <p className="text-center">Mathematics</p>
+        </div>
+        <div className="col-auto d-flex flex-column align-items-center">
+          <FontAwesomeIcon icon={faMicroscope} />
+          <p className="text-center">Biology</p>
+        </div>
+        <div className="col-auto d-flex flex-column align-items-center">
+          <FontAwesomeIcon icon={faHeartbeat} />
+          <p className="text-center">Health</p>
+        </div>
+        <div className="col-auto d-flex flex-column align-items-center">
+          <FontAwesomeIcon icon={faPalette} />
+          <p className="text-center">Art</p>
+        </div>
+        <div className="col-auto d-flex flex-column align-items-center">
+          <FontAwesomeIcon icon={faChartLine} />
+          <p className="text-center">Business</p>
+        </div>
+        <div className="col-auto d-flex flex-column align-items-center">
+          <FontAwesomeIcon icon={faBalanceScale} />
+          <p className="text-center">Law</p>
+        </div>
+        <div className="col-auto d-flex flex-column align-items-center">
+          <FontAwesomeIcon icon={faDollarSign} />
+          <p className="text-center">Investment</p>
+        </div>
+      </div>
+
+
+      <div className="row justify-content-center">
         <div className="col-auto d-flex flex-column align-items-center">
           <FontAwesomeIcon icon={faSquareRootAlt} />
           <p className="text-center">Mathematics</p>
@@ -46,7 +84,8 @@ export default function Subjects() {
           <p className="text-center">Investment</p>
         </div>
       </div>
-
+      <div className="row border border-dark my-4"></div>
+      <h1 className="text-center mb-2 fw-bold">Subject</h1>
     </div>
   );
 }
