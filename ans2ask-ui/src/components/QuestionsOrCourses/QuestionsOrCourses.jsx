6068af4c671b7fbe2866815d6ef7b064ca8b8476
Miguel Garza Carranza <maiky@meta.com>
2023-07-07 17:29:48 -0700

    Added the icons of the subject section

--- ans2ask-ui/src/components/QuestionsOrCourses/QuestionsOrCourses.jsx
+++ ans2ask-ui/src/components/QuestionsOrCourses/QuestionsOrCourses.jsx
@@ -6,7 +6,7 @@ export default function QuestionsOrCourses() {
     <div className="QuestionsOrCourses">
         <div className="d-flex">
             <button className="btn btn-dark w-100 d-block fw-bold mx-4 mt-4 mb-1 p-2"> <h4>Questions</h4> </button>
-            <button className="btn btn-dark w-100 d-block fw-bold mx-4 mt-4 mb-1 p-2"> <h4>Courses</h4> </button>
+            <button className="btn btn-outline-dark w-100 d-block fw-bold mx-4 mt-4 mb-1 p-2"> <h4>Courses</h4> </button>
         </div>
     </div>
   );
--- ans2ask-ui/src/components/Subjects/Subjects.jsx
+++ ans2ask-ui/src/components/Subjects/Subjects.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBrain, faSquareRootAlt, faCode, faGlobe, faMicroscope, faHeartbeat, faPalette, faChartLine, faBalanceScale, faDollarSign } from "@fortawesome/free-solid-svg-icons";
+import { faBrain, faSquareRootAlt, faCode, faMicroscope, faHeartbeat, faPalette, faChartLine, faBalanceScale, faDollarSign } from "@fortawesome/free-solid-svg-icons";
+import { faGamepad, faScroll, faFlask, faBolt, faFilm, faGlobeAmericas, faBook, faUtensils, faLanguage } from "@fortawesome/free-solid-svg-icons";
 import "./Subjects.css";
 
 export default function Subjects() {
   return (
     <div className="subjects">
-      <div className="row justify-content-center mt-2">
-      <div className="col-auto d-flex flex-column align-items-center">
+      <div className="row justify-content-center mt-2 mb-2">
+        <div className="col-auto d-flex flex-column align-items-center">
           <FontAwesomeIcon icon={faBrain} />
           <p className="text-center">All</p>
         </div>
@@ -45,46 +46,47 @@ export default function Subjects() {
         </div>
       </div>
 
-
       <div className="row justify-content-center">
         <div className="col-auto d-flex flex-column align-items-center">
-          <FontAwesomeIcon icon={faSquareRootAlt} />
-          <p className="text-center">Mathematics</p>
-        </div>
-        <div className="col-auto d-flex flex-column align-items-center">
-          <FontAwesomeIcon icon={faCode} />
-          <p className="text-center">Informatics</p>
-        </div>
-        <div className="col-auto d-flex flex-column align-items-center">
-          <FontAwesomeIcon icon={faGlobe} />
+          <FontAwesomeIcon icon={faScroll} />
           <p className="text-center">History</p>
         </div>
         <div className="col-auto d-flex flex-column align-items-center">
-          <FontAwesomeIcon icon={faMicroscope} />
-          <p className="text-center">Biology</p>
+          <FontAwesomeIcon icon={faGamepad} />
+          <p className="text-center">Videogames</p>
+        </div>        
+        <div className="col-auto d-flex flex-column align-items-center">
+          <FontAwesomeIcon icon={faFlask} />
+          <p className="text-center">Chemistry</p>
         </div>
         <div className="col-auto d-flex flex-column align-items-center">
-          <FontAwesomeIcon icon={faHeartbeat} />
-          <p className="text-center">Health</p>
+          <FontAwesomeIcon icon={faBolt} />
+          <p className="text-center">Physics</p>
         </div>
         <div className="col-auto d-flex flex-column align-items-center">
-          <FontAwesomeIcon icon={faPalette} />
-          <p className="text-center">Art</p>
+          <FontAwesomeIcon icon={faFilm} />
+          <p className="text-center">Animation</p>
         </div>
         <div className="col-auto d-flex flex-column align-items-center">
-          <FontAwesomeIcon icon={faChartLine} />
-          <p className="text-center">Business</p>
+          <FontAwesomeIcon icon={faGlobeAmericas} />
+          <p className="text-center">Geography</p>
         </div>
         <div className="col-auto d-flex flex-column align-items-center">
-          <FontAwesomeIcon icon={faBalanceScale} />
-          <p className="text-center">Law</p>
+          <FontAwesomeIcon icon={faBook} />
+          <p className="text-center">SAT</p>
         </div>
         <div className="col-auto d-flex flex-column align-items-center">
-          <FontAwesomeIcon icon={faDollarSign} />
-          <p className="text-center">Investment</p>
+          <FontAwesomeIcon icon={faUtensils} />
+          <p className="text-center">Food</p>
+        </div>
+        <div className="col-auto d-flex flex-column align-items-center">
+          <FontAwesomeIcon icon={faLanguage} />
+          <p className="text-center">Languages</p>
         </div>
       </div>
+
       <div className="row border border-dark my-4"></div>
+      
       <h1 className="text-center mb-2 fw-bold">Subject</h1>
     </div>
   );
