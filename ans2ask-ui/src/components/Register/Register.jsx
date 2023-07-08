d87b499875354259eacd16313259308b8aca8f49
Miguel Garza Carranza <maiky@meta.com>
2023-07-08 14:01:41 -0700

    Fully integrated the frontend and backend of the Register component

--- ans2ask-ui/src/components/Register/Register.jsx
+++ ans2ask-ui/src/components/Register/Register.jsx
@@ -1,8 +1,56 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useContext } from 'react';
+import { Link, useNavigate } from "react-router-dom";
+// import ----- { UserContext } from '../../UserContext.js';
 import "./Register.css";
 
 export default function Register() {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    // ----- const { updateUser } = useContext(UserContext);
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    
+        try {
+          // Make the signup API request
+          const response = await fetch(`http://localhost:3001/users`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, email, password }),
+            credentials: 'include'
+          });
+    
+          if (response.ok) {
+            const data = await response.json();
+            const loggedInUser = data.user;
+    
+            console.log('The user was successfully registered');
+    
+            // Reset form fields
+            setUsername('');
+            setEmail('');
+            setPassword('');
+    
+            // Update the user context
+            // ----------- updateUser(loggedInUser);
+    
+            // Navigate to the home page after successful login
+            navigate('/login');
+          } else {
+            // Handle signup failure case
+            alert('Registration failed');
+          }
+        } catch (error) {
+          // Handle any network or API request errors
+          alert('Registration failed: ' + error);
+        }
+      };
+    
     return (
       <div className="register">
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -19,23 +67,42 @@ export default function Register() {
         <div className="d-flex justify-content-center align-items-center custom-margin-register">
             <div className="custom-container bg-light p-4 border rounded px-5">
                 <h1 className="text-center mb-4 fw-bold">Welcome to Ans2Ask</h1>
-                <form>
+                <form onSubmit={handleSubmit}>
                 <div className="form-group mb-4">
                     <label className="mb-2 fw-bold" htmlFor="username">Username</label>
-                    <input type="text" className="form-control bg-lighter" id="username" placeholder="" />
+                    <input
+                        className="form-control bg-lighter" 
+                        type="text"
+                        id="username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        required
+                    />
                 </div>
                 <div className="form-group mb-4">
                     <label className="mb-2 fw-bold" htmlFor="email">Email</label>
-                    <input type="email" className="form-control bg-lighter" id="email" placeholder="" />
+                    <input 
+                        className="form-control bg-lighter" 
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
                 </div>
                 <div className="form-group mb-5">
                     <label className="mb-2 fw-bold" htmlFor="password">Password</label>
-                    <input type="password" className="form-control bg-lighter" id="password" placeholder="" />
+                    <input 
+                        className="form-control bg-lighter"  
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
                 </div>
                 <div className="text-center">
-                    <Link to={`/register`} className="btn btn-dark w-100 d-block fw-bold">
-                        Register
-                    </Link>
+                    <button className="btn btn-dark w-100 d-block fw-bold">Register</button>
                     <div className="mt-3">
                         <a className="custom-link" href="/login" >Already have an account?</a>
                     </div>
