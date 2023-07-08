adc98a25a2012b2bd62077bd9fae640c95da9188
Miguel Garza Carranza <maiky@meta.com>
2023-07-08 15:46:31 -0700

    Added a dropdown on the FaUser icon at the navbar

--- ans2ask-ui/src/components/Navbar/Navbar.jsx
+++ ans2ask-ui/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { NavDropdown } from 'react-bootstrap';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBell, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faBell, faUser, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+import 'bootstrap/dist/css/bootstrap.css';
 import "./Navbar.css";
 
 export default function Navbar() {
@@ -17,9 +19,11 @@ export default function Navbar() {
                         <div style={{ marginLeft: "1.75rem" }}>
                             <FontAwesomeIcon icon={faBell} />
                         </div>
-                        <div style={{ marginLeft: "1.75rem" }}>
-                            <FontAwesomeIcon icon={faUser} />
-                        </div>
+                        <NavDropdown style={{ marginLeft: "1.75rem" }} alignRight title={<FontAwesomeIcon icon={faUser} />} id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#profile">View Profile</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#logout">Log Out &nbsp; <FontAwesomeIcon icon={faSignOutAlt} /></NavDropdown.Item>
+                        </NavDropdown>
                     </div>
                 </div>
             </div>
