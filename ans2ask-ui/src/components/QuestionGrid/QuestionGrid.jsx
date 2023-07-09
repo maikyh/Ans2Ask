131ded8590c95c9bef45f0211d47d138c7378a26
Miguel Garza Carranza <maiky@meta.com>
2023-07-08 17:41:40 -0700

    Implemented the fetch of the questions route

--- ans2ask-ui/src/components/QuestionGrid/QuestionGrid.jsx
+++ ans2ask-ui/src/components/QuestionGrid/QuestionGrid.jsx
@@ -1,13 +1,29 @@
 import React from "react";
+import { useState, useEffect, useContext } from "react";
 import Question from "../Question/Question";
 import "./QuestionGrid.css";
 
 export default function QuestionGrid() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('http://localhost:3001/questions');
+      const data = await response.json();
+      setQuestions(data);
+    };
+    fetchPosts();
+  }, []);
+
+  console.log(questions);
+
   return (
     <div className="QuestionGrid">
-        <Question id={1} username={"Miguel"} subject={"Subject"} />
-        <Question id={2} username={"Diego"} subject={"Subject"} />
-        <Question id={3} username={"Finn"} subject={"Subject"} />
+      {questions?.map((question) => (
+        <div key={question.id}>
+          <Question id={question.id} username={question.user.username} subject={question.subject} title={question.title} text={question.text} />
+        </div>
+      ))}
     </div>
   );
 }
