d9e7d584b7cfd0f4033bc908d15ec389f7d10b2e
Miguel Garza Carranza <maiky@meta.com>
2023-07-11 11:23:40 -0700

    Added relations of answers in index.js

--- ans2ask-api/models/index.js
+++ ans2ask-api/models/index.js
@@ -1,7 +1,11 @@
 import { User } from './user.js';
 import { Question } from './question.js';
+import { Answer } from './answer.js';
 
 User.hasMany(Question, { as: 'questions', foreignKey: 'userId' });
 Question.belongsTo(User, { as: 'user', foreignKey: 'userId' });
 
-export { User, Question };
+User.hasMany(Answer, { as: 'answers', foreignKey: 'userId' });
+Answer.belongsTo(User, { as: 'user', foreignKey: 'userId' });
+
+export { User, Question, Answer };
