fcc6c30106ba40aa41ddb5a0d2ad0485819ecc6d
Miguel Garza Carranza <maiky@meta.com>
2023-07-11 19:40:58 -0700

    Implemented frontend of the indivual question

--- ans2ask-ui/src/components/QuestionDetails/QuestionDetails.jsx
+++ ans2ask-ui/src/components/QuestionDetails/QuestionDetails.jsx
@@ -3,6 +3,8 @@ import { useState, useEffect, useContext } from "react";
 import { useParams } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
 import { UserContext } from "../../UserContext.js";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer.jsx";
 import "./QuestionDetails.css";
@@ -42,7 +44,32 @@ export default function QuestionDetails({handleSetSearchQuery}) {
             <Navbar handleSetSearchQuery={handleSetSearchQuery} handleLogout={handleLogout}/>
             <div className="d-flex justify-content-center align-items-center">
                 <div className="custom-container-question-details bg-light px-4 pt-4 pb-2">
-                    lol
+                <div className="question-card bg-white mt-4 p-3">
+                <div className="row">
+                <div className="col-auto">
+                    <FontAwesomeIcon icon={faUser} />
+                </div>
+
+                <div className="col-auto">
+                    <h6 className="mt-1"> {user.username} </h6>
+                </div>
+
+                <div className="col-auto"> <h6 className="mt-1"> - </h6> </div>
+
+                <div className="col-auto">
+                    <h6 className="mt-1"> {question.subject} </h6>
+                </div>
+                <div>
+
+                </div>
+                </div>
+                <div>
+                <span className="fw-bold">{question.title}</span>
+                </div>
+                <div className="">
+                <p> {question.body} </p>
+                </div>
+            </div>
                 </div>
             </div>
             <Footer/>
