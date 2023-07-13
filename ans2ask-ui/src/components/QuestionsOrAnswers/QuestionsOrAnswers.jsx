48b45fc46c7d8b22ab0d8b8c589e427ff2f728eb
Miguel Garza Carranza <maiky@meta.com>
2023-07-13 12:00:56 -0700

    Reduced the margin top of Options on user Profile

--- ans2ask-ui/src/components/QuestionsOrAnswers/QuestionsOrAnswers.jsx
+++ ans2ask-ui/src/components/QuestionsOrAnswers/QuestionsOrAnswers.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import "./QuestionsOrAnswers.css";
 
 export default function QuestionsOrAnswers({selectedOption, handleSetSelectedOption}) {
-  const active = "btn btn-dark w-100 d-block fw-bold mx-4 mt-4 mb-1 p-2";
-  const inactive = "btn btn-outline-dark w-100 d-block fw-bold mx-4 mt-4 mb-1 p-2";
+  const active = "btn btn-dark w-100 d-block fw-bold mx-4 mt-0 mb-1 p-2";
+  const inactive = "btn btn-outline-dark w-100 d-block fw-bold mx-4 mt-0 mb-1 p-2";
 
   return (
     <div className="QuestionsOrCourses">
