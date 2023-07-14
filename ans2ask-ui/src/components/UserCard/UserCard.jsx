f6bbdd74d08f95264748708eb5e70f3603b663c8
Miguel Garza Carranza <maiky@meta.com>
2023-07-13 22:05:18 -0700

    Erased unused classes

--- ans2ask-ui/src/components/UserCard/UserCard.jsx
+++ ans2ask-ui/src/components/UserCard/UserCard.jsx
@@ -9,9 +9,9 @@ export default function UserCard({ username, email }) {
       <div className="card-body d-flex align-items-center">
         <FontAwesomeIcon className="fa-10x user-icon m-5" icon={faUser} />
         <div>
-          <h5 className="card-title mb-0">{username}</h5>
+          <h5 className="mb-0">{username}</h5>
           <h6 className="mb-0"> MetaU Intern </h6>
-          <p className="card-text mb-2">{email}</p>
+          <p className="mb-2">{email}</p>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus sint et beatae laboriosam magnam quibusdam! Praesentium atque ullam, laudantium commodi numquam iure unde voluptatem veniam dolor magnam minus quo odit enim, natus debitis sit, eum doloremque dolore possimus veritatis totam? Error, reprehenderit similique! Sit excepturi velit accusamus culpa temporibus? Doloremque?</p>
         </div>
       </div>
