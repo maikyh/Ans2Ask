12da5230ee26a29b3733e85a78270add13c0c5a4
Miguel Garza Carranza <maiky@meta.com>
2023-07-14 12:56:26 -0700

    Moved all answer routes from express.js to routes/answers.js

---
+++ ans2ask-api/routes/answers.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import { User } from '../models/index.js';
+import { Answer } from '../models/answer.js';
+const router = express.Router();
+
+// Route to get all answers, with associated users
+router.get('/answers', async (req, res) => {
+    try {
+        const answers = await Answer.findAll({
+        include: [{ model: User, as: 'user' }],
+        order: [['createdAt', 'DESC']]
+        });
+        res.json(answers);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// Route to create a new answer
+router.post('/answers', async (req, res) => {
+    try {
+        // Check if user is logged in
+        if (!req.session.user) {
+        return res.status(401).json({ error: 'Unauthorized' });
+        }
+
+        // Retrieve the current user from the session
+        const currentUser = req.session.user;
+
+        // Create the answer with the current user ID
+        const answer = await Answer.create({
+        ...req.body,
+        userId: currentUser.id
+        });
+
+        const answerWithUser = await Answer.findOne({
+        where: { id: answer.id },
+        include: [{ model: User, as: 'user' }]
+        });
+
+        res.status(201).json(answerWithUser);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+export default router;
