36ef2c7824582a3934392182b358e44cb07aa848
Miguel Garza Carranza <maiky@meta.com>
2023-07-13 21:17:04 -0700

    Added some boostrap style on UserCard

--- ans2ask-ui/src/components/UserCard/UserCard.jsx
+++ ans2ask-ui/src/components/UserCard/UserCard.jsx
@@ -3,15 +3,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import "./UserCard.css";
 
-export default function UserCard({username, email}) {
-
+export default function UserCard({ username, email }) {
   return (
-    <div className="UserCard">
-        <div className="d-flex">
-            <FontAwesomeIcon className="fa-10x" icon={faUser} />
-                {username}
-                {email}
+    <div className="UserCard card">
+      <div className="card-body d-flex align-items-center">
+        <FontAwesomeIcon className="fa-10x user-icon m-5" icon={faUser} />
+        <div>
+          <h5 className="card-title mb-0">{username}</h5>
+          <p className="card-text">{email}</p>
         </div>
+      </div>
     </div>
   );
 }
