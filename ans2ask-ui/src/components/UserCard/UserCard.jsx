45fa2d97fe04cd974189d21da9d196fd1b23d9fc
Miguel Garza Carranza <maiky@meta.com>
2023-07-14 17:29:41 -0700

    Created the edit button

--- ans2ask-ui/src/components/UserCard/UserCard.jsx
+++ ans2ask-ui/src/components/UserCard/UserCard.jsx
@@ -6,18 +6,65 @@ import {
   EditableInput,
   EditableTextarea,
   EditablePreview,
-  IconButton
+  IconButton,
+  ButtonGroup,
+  Input,
+  Flex,
+  useEditableControls
 } from '@chakra-ui/react' 
 import "./UserCard.css";
 
 import { EditIcon, CheckIcon, CloseIcon } from '@chakra-ui/icons'
 
 export default function UserCard({ username, title, email, about, coins }) {
+  function EditableControls() {
+    const {
+      isEditing,
+      getSubmitButtonProps,
+      getCancelButtonProps,
+      getEditButtonProps,
+    } = useEditableControls()
+
+    return isEditing ? (
+      <ButtonGroup justifyContent='center' size='sm'>
+        <IconButton icon={<CheckIcon />} {...getSubmitButtonProps()} />
+        <IconButton icon={<CloseIcon />} {...getCancelButtonProps()} />
+      </ButtonGroup>
+    ) : (
+      <Flex justifyContent='center'>
+        <IconButton size='sm' icon={<EditIcon />} {...getEditButtonProps()} />
+      </Flex>
+    )
+  }
+
   return (
     <div className="UserCard card">
       <div className="card-body d-flex align-items-center">
         <FontAwesomeIcon className="fa-10x user-icon m-5" icon={faUser} />
         <div>
+          
+        <div className="row">
+          <div className="col d-flex align-items-center">
+          <Editable
+            textAlign='center'
+            defaultValue={username}
+            fontSize='calc(1.325rem + .9vw)'
+            isPreviewFocusable={false}
+          >
+            {/* Here is the custom input */}
+            <div className="row">
+              <div className="col d-flex align-items-center">
+                <EditablePreview />
+                <Input style={{fontSize: 'calc(1.325rem + .9vw)'}} as={EditableInput} />  
+              </div>
+              <div className="col-auto d-flex align-items-center">
+                <EditableControls />
+              </div>
+            </div>
+          </Editable>
+          </div>
+        </div>
+
           <h2 className="mb-0">{username}</h2>
           <h5 className="mb-0"> {title} </h5>
           <p className="mb-0">{email}</p>
