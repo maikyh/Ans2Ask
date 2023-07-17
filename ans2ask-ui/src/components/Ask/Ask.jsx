0efab698de3ee45a0692ba44bf753111d7424949
Miguel Garza Carranza <maiky@meta.com>
2023-07-17 12:26:49 -0700

    Integrated new data reqs when asking a question

--- ans2ask-ui/src/components/Ask/Ask.jsx
+++ ans2ask-ui/src/components/Ask/Ask.jsx
@@ -51,7 +51,7 @@ export default function Ask({handleSetSearchQuery}) {
             headers: {
               'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ title, body, subject }),
+            body: JSON.stringify({ title, body, subject, coins }),
             credentials: 'include'
           });
     
