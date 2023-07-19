a57bd6ecd45b99c15a1d65d74b238e088833c6cc
Miguel Garza Carranza <maiky@meta.com>
2023-07-19 15:31:47 -0700

    Made subFunctions for the scrape of titles and url links

--- ans2ask-api/routes/googleScraping.js
+++ ans2ask-api/routes/googleScraping.js
@@ -12,27 +12,40 @@ const getDataFromGoogleSearchResults = (query) => {
                 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36, Custom-Agent/1.0"
         })
         .then((response) => {
-            let $ = cheerio.load(response.body);
+            const $ = cheerio.load(response.body);
 
-            let titles = [];
-            let links = [];
-
-            $(".DhN8Cf > a > h3").each((i, el) => {
-                titles[i] = $(el).text();
-            });
-            $(".DhN8Cf > a").each((i, el) => {
-                links[i] = $(el).attr("href");
-            });
-
-            const dataFromGoogleSearchResults = [];
-
-            for (let i = 0; i < titles.length; i++) {
-                dataFromGoogleSearchResults[i] = {
-                    title: titles[i],
-                    link: links[i],
-                };
+            function getTitlesFromResponse() {
+                const titles = [];
+                $(".DhN8Cf > a > h3").each((i, el) => {
+                  titles.push($(el).text());
+                });
+                return titles;
             }
-            return dataFromGoogleSearchResults;
+              
+            function getLinksFromResponse() {
+                const links = [];
+                $(".DhN8Cf > a").each((i, el) => {
+                    links.push($(el).attr("href"));
+                });
+                return links;
+            }
+
+            const titles = getTitlesFromResponse();
+            const links = getLinksFromResponse();
+
+            function getDataFromGoogleSearchResultsResponse() {
+                const dataFromGoogleSearchResults = [];
+                for (let i = 0; i < titles.length; i++) {
+                    dataFromGoogleSearchResults[i] = {
+                        title: titles[i],
+                        link: links[i],
+                    };
+                }
+                return dataFromGoogleSearchResults;
+            }
+
+            const dataFromGoogleSearchResults = getDataFromGoogleSearchResultsResponse();
+            return dataFromGoogleSearchResults
         });
 };
 
