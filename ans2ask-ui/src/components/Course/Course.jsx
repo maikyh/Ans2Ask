57482a117b7ed22425201db02539ce768e4c3d4b
Miguel Garza Carranza <maiky@meta.com>
2023-07-19 12:05:23 -0700

    Added youtube embed video into my course component

--- ans2ask-ui/src/components/Course/Course.jsx
+++ ans2ask-ui/src/components/Course/Course.jsx
@@ -4,6 +4,16 @@ import "./Course.css";
 export default function QuestionGrid({video}) {
   return (
     <div className="Course">
+        <div key={video.videoDetails.id.videoId} className="my-2">
+              <iframe
+                width="560"
+                height="315"
+                src={`https://www.youtube.com/embed/${video.videoDetails.id.videoId}`}
+                title="YouTube Video Player"
+                frameBorder="0"
+                allowFullScreen
+              />
+            </div>
         <div className="card" style={{ width: '18rem' }}>
             <img src={video.channel.thumbnail} className="card-img-top" alt={video.channel.name} />
             <div className="card-body">
