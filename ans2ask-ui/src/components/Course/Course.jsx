05774616b9fdf06976852e6403044f745dabbc84
Miguel Garza Carranza <maiky@meta.com>
2023-07-19 12:08:46 -0700

    Styled the Course card component

--- ans2ask-ui/src/components/Course/Course.jsx
+++ ans2ask-ui/src/components/Course/Course.jsx
@@ -4,26 +4,30 @@ import "./Course.css";
 export default function QuestionGrid({video}) {
   return (
     <div className="Course">
-        <div key={video.videoDetails.id.videoId} className="my-2">
-              <iframe
-                width="560"
+        <div className="card mt-4" style={{ width: '900px' }}>
+            <div className="row no-gutters">
+            <div className="col-md-6">
+                <iframe
+                width="100%"
                 height="315"
                 src={`https://www.youtube.com/embed/${video.videoDetails.id.videoId}`}
                 title="YouTube Video Player"
                 frameBorder="0"
                 allowFullScreen
-              />
+                />
             </div>
-        <div className="card" style={{ width: '18rem' }}>
-            <img src={video.channel.thumbnail} className="card-img-top" alt={video.channel.name} />
-            <div className="card-body">
+            <div className="col-md-6">
+                <div className="card-body">
                 <h5 className="card-title">{video.title}</h5>
                 <p className="card-text">
                     {video.views} | {video.date} | Duration: {video.duration}
                 </p>
                 <a href={video.channel.link} className="btn btn-primary">Visit Channel</a>
+                </div>
+            </div>
             </div>
         </div>
     </div>
+
   );
 }
