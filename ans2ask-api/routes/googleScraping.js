74459dbfe63c1898f36933ba1a6287a3ca569334
Miguel Garza Carranza <maiky@meta.com>
2023-07-18 21:14:30 -0700

    Implemented route to fetch data results

--- ans2ask-api/routes/googleScraping.js
+++ ans2ask-api/routes/googleScraping.js
@@ -1,37 +1,50 @@
 import unirest from "unirest";
 import cheerio from "cheerio";
+import express from 'express';
 
-const getDataFromGoogleSearchResults = () => {
-  return unirest
-    .get("https://www.google.com/search?q=Mathematics+youtube+courses&tbm=vid")
-    .headers({
-      "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36",
-    })
-    .then((response) => {
-      let $ = cheerio.load(response.body);
+const router = express.Router();
 
-      //Best Youtube videos (Sorted by Google's Ranking Search Algo)
-      let titles = [];
-      let links = [];
+const getDataFromGoogleSearchResults = (query) => {
+    return unirest
+        .get(`https://www.google.com/search?q=${query}+youtube+courses&tbm=vid`)
+        .headers({
+            "User-Agent":
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36",
+        })
+        .then((response) => {
+            let $ = cheerio.load(response.body);
 
-      $(".DhN8Cf > a > h3").each((i, el) => {
-        titles[i] = $(el).text();
-      });
-      $(".DhN8Cf > a").each((i, el) => {
-        links[i] = $(el).attr("href");
-      });
+            let titles = [];
+            let links = [];
 
-      const dataFromGoogleSearchResults = [];
+            $(".DhN8Cf > a > h3").each((i, el) => {
+                titles[i] = $(el).text();
+            });
+            $(".DhN8Cf > a").each((i, el) => {
+                links[i] = $(el).attr("href");
+            });
 
-      for (let i = 0; i < titles.length; i++) {
-        dataFromGoogleSearchResults[i] = {
-          title: titles[i],
-          links: links[i],
-        };
-      }
-      console.log(dataFromGoogleSearchResults)
-    });
+            const dataFromGoogleSearchResults = [];
+
+            for (let i = 0; i < titles.length; i++) {
+                dataFromGoogleSearchResults[i] = {
+                    title: titles[i],
+                    links: links[i],
+                };
+            }
+            return dataFromGoogleSearchResults;
+        });
 };
 
-getDataFromGoogleSearchResults();
+// Route to get Best Youtube Courses depending on query (Subject)
+router.get('/google/:query', async (req, res) => {
+    try {
+        const query = req.params.query;
+        const data = await getDataFromGoogleSearchResults(query);
+        res.json(data);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+export default router;
