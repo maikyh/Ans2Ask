d028bd031c6a2ada3508eae1c0cc5d379ea8160e
Miguel Garza Carranza <maiky@meta.com>
2023-07-22 19:06:10 -0700

    Changed btn style

--- ans2ask-ui/src/utils/StatusQC.jsx
+++ ans2ask-ui/src/utils/StatusQC.jsx
@@ -1,8 +1,8 @@
 const Status = {
-    activeQuestions: "btn btn-primary w-100 d-block fw-bold mx-4 mt-0 mb-1 p-2",
-    inactiveQuestions: "btn btn-outline-primary w-100 d-block fw-bold mx-4 mt-0 mb-1 p-2",
-    activeCourses: "btn btn-danger w-100 d-block fw-bold mx-4 mt-0 mb-1 p-2",
-    inactiveCourses: "btn btn-outline-danger w-100 d-block fw-bold mx-4 mt-0 mb-1 p-2"    
+    activeQuestions: "btn btn-dark w-100 d-block fw-bold mx-4 mt-0 mb-1 p-2",
+    inactiveQuestions: "btn btn-outline-dark w-100 d-block fw-bold mx-4 mt-0 mb-1 p-2",
+    activeCourses: "btn btn-dark w-100 d-block fw-bold mx-4 mt-0 mb-1 p-2",
+    inactiveCourses: "btn btn-outline-dark w-100 d-block fw-bold mx-4 mt-0 mb-1 p-2"    
 }
 
 export default Status;
