1abd3d66ba9571528ec215490af587530ebfe327
Miguel Garza Carranza <maiky@meta.com>
2023-07-22 18:57:18 -0700

    Created Status util to Questions and Courses buttons

---
+++ ans2ask-ui/src/utils/StatusQC.jsx
@@ -0,0 +1,8 @@
+const Status = {
+    activeQuestions: "btn btn-primary w-100 d-block fw-bold mx-4 mt-0 mb-1 p-2",
+    inactiveQuestions: "btn btn-outline-primary w-100 d-block fw-bold mx-4 mt-0 mb-1 p-2",
+    activeCourses: "btn btn-danger w-100 d-block fw-bold mx-4 mt-0 mb-1 p-2",
+    inactiveCourses: "btn btn-outline-danger w-100 d-block fw-bold mx-4 mt-0 mb-1 p-2"    
+}
+
+export default Status;
