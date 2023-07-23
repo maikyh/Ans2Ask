9dc5a1d358b02587e4e150a8bc11ab22b91f2615
Miguel Garza Carranza <maiky@meta.com>
2023-07-22 18:58:36 -0700

    Implemented util in QuestionsOrCourses.jsx

--- ans2ask-ui/src/components/QuestionsOrCourses/QuestionsOrCourses.jsx
+++ ans2ask-ui/src/components/QuestionsOrCourses/QuestionsOrCourses.jsx
@@ -1,15 +1,29 @@
 import React from "react";
-import Options from "../../utils/OptionsQC.jsx"
-import Status from "../../utils/Status.jsx"
+import Options from "../../utils/OptionsQC.jsx";
+import Status from "../../utils/StatusQC.jsx";
 import "./QuestionsOrCourses.css";
 
-export default function QuestionsOrCourses({selectedOption, handleSetSelectedOption}) {
+const QuestionsOrCourses = ({ selectedOption, handleSetSelectedOption }) => {
   return (
     <div className="QuestionsOrCourses">
-        <div className="d-flex">
-            <button onClick={() => handleSetSelectedOption(1)} className={selectedOption === Options.question ? Status.active : Status.inactive}> <h4>Questions</h4> </button>
-            <button onClick={() => handleSetSelectedOption(2)} className={selectedOption === Options.course ? Status.active : Status.inactive}> <h4>Courses</h4> </button>
-        </div>
+      <div className="d-flex">
+        <button
+          onClick={() => handleSetSelectedOption(1)}
+          className={selectedOption === Options.question ? Status.activeQuestions : Status.inactiveQuestions}
+          data-tooltip="MOST RECENT QUESTIONS"
+        >
+          <h4>Questions</h4>
+        </button>
+        <button
+          onClick={() => handleSetSelectedOption(2)}
+          className={selectedOption === Options.course ? Status.activeCourses : Status.inactiveCourses}
+          data-tooltip="BEST YOUTUBE COURSES (Google/Youtube Scraping)"
+        >
+          <h4>Courses</h4>
+        </button>
+      </div>
     </div>
   );
-}
+};
+
+export default QuestionsOrCourses;
