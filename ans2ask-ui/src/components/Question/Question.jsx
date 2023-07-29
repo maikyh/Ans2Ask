ce67e32f7d2351dfa9afca50b9f1ac7f1111c5d6
Miguel Garza Carranza <maiky@meta.com>
2023-07-29 02:07:18 -0700

    Implemented darkMode on each individual question

--- ans2ask-ui/src/components/Question/Question.jsx
+++ ans2ask-ui/src/components/Question/Question.jsx
@@ -1,5 +1,6 @@
 import React from "react";  
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import { UserContext } from '../../UserContext.js';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
@@ -11,6 +12,7 @@ const MAX_LENGTH = 369;
 
 const Question = ({sentence, images, id, username, email, userTitle, subject, title, body, coins}) => {
   const [answers, setAnswers] = useState([]);
+  const { darkMode } = useContext(UserContext);
 
   const image = images.filter(image => image.public_id === email);
 
@@ -39,7 +41,7 @@ const Question = ({sentence, images, id, username, email, userTitle, subject, ti
   
   return (
     <div style={{ cursor: 'pointer', position: 'relative' }} className="question" onClick={() => handleNavigateToQuestionDetails(id)}>
-      <div style={{border: '0.9px solid gray' }} className="question-card bg-white mt-4 p-3">
+      <div style={{ backgroundColor: darkMode ? "#2D3748" : "rgba(248,249,250,1)", border: '0.9px solid gray' }} className="question-card mt-4 p-3">
         <div className="row">
           <div className="col-auto">
             <div className='preview-container' style={{width: "32px", height: "32px", marginBottom: "8px"}}>
@@ -49,31 +51,32 @@ const Question = ({sentence, images, id, username, email, userTitle, subject, ti
             </div>
           </div>
 
-          <div className="col-auto">
-            <h6 className="mt-1"> {username} </h6>
+          <div className="col-auto" style={{color: darkMode ? "rgba(255, 255, 255, 0.92)" : "rgba(0,0,0,1)"}}>
+            <h6 className="mt-1" style={{color: darkMode ? "rgba(255, 255, 255, 0.92)" : "rgba(0,0,0,1)"}}> {username} </h6>
           </div>
 
-          <div className="col-auto"> <h6 className="mt-1"> - </h6> </div>
+          <div className="col-auto" style={{color: darkMode ? "rgba(255, 255, 255, 0.92)" : "rgba(0,0,0,1)"}}> <h6 className="mt-1"> - </h6> </div>
 
           <div className="col-auto">
-            <h6 className="mt-1" style={{ fontStyle: "italic" }}> {userTitle} </h6>
+            <h6 className="mt-1" style={{color: darkMode ? "rgba(255, 255, 255, 0.92)" : "rgba(0,0,0,1)", fontStyle: "italic" }}> {userTitle} </h6>
           </div>
 
-          <div className="col-auto"> <h6 className="mt-1"> - </h6> </div>
+          <div className="col-auto" style={{color: darkMode ? "rgba(255, 255, 255, 0.92)" : "rgba(0,0,0,1)"}}> <h6 className="mt-1"> - </h6> </div>
 
           <div className="col-auto">
-            <h6 className="mt-1 underline-text"> {subject} </h6>
+            <h6 className="mt-1 underline-text" style={{color: darkMode ? "rgba(255, 255, 255, 0.92)" : "rgba(0,0,0,1)"}}> {subject} </h6>
           </div>
           <div>
 
           </div>
         </div>
         <div>
-          <span className="fw-bold">{title}</span>
+          <span className="fw-bold" style={{color: darkMode ? "rgba(255, 255, 255, 0.92)" : "rgba(0,0,0,1)"}}>{title}</span>
         </div>
         <div className="">
           <div style={{marginBottom:"10px"}}>
             <Highlighter
+              style={{color: darkMode ? "rgba(255, 255, 255, 0.92)" : "rgba(0,0,0,1)"}}
               highlightClassName="YourHighlightClass"
               searchWords={sentence ? sentence.split(' ') : []}
               autoEscape={true}
@@ -82,13 +85,13 @@ const Question = ({sentence, images, id, username, email, userTitle, subject, ti
           </div>
           {
             answersOfCurrentQuestion.length > 0 && 
-            <div className="position-absolute bottom-0 end-0 p-1 px-3 text-dark underline-text">
+            <div className="position-absolute bottom-0 end-0 p-1 px-3 underline-text" style={{color: darkMode ? "rgba(255, 255, 255, 0.92)" : "rgba(0,0,0,1)"}}>
                 {answersOfCurrentQuestion.length} answers
             </div>
           }
           {
             answersOfCurrentQuestion.length == 0 && 
-            <div className="position-absolute bottom-0 end-0 p-1 px-3 text-dark underline-text">
+            <div className="position-absolute bottom-0 end-0 p-1 px-3 underline-text" style={{color: darkMode ? "rgba(255, 255, 255, 0.92)" : "rgba(0,0,0,1)"}}>
                 No answers, be the first!
             </div>
           }
