5cb82e2db38ab9c1fc3f1ca801110cd1e6285a48
Miguel Garza Carranza <maiky@meta.com>
2023-07-28 22:22:30 -0700

    Minor changes

--- ans2ask-ui/src/components/Footer/Footer.jsx
+++ ans2ask-ui/src/components/Footer/Footer.jsx
@@ -6,9 +6,9 @@ const Footer = () => {
     const { darkMode } = useContext(UserContext);
     
     return (
-        <footer className="fixed-bottom" style={{ backgroundColor: darkMode ? "#2D3748" : "rgba(248,249,250,1)", height:"72px"}}>
+        <footer className="fixed-bottom" style={{ backgroundColor: darkMode ? "#2D3748" : "rgba(248,249,250,1)", height:"65px"}}>
             <div className="container text-center">
-                <p style={{color: darkMode ? "rgba(255, 255, 255, 0.92)" : "rgba(0,0,0,1)", paddingTop: "25px"}} className="mb-0">
+                <p style={{color: darkMode ? "rgba(255, 255, 255, 0.92)" : "rgba(0,0,0,1)", paddingTop: "25px"}} className="m-0">
                     &copy; {new Date().getFullYear()} Ans2Ask. All rights reserved.
                 </p>
             </div>
