1f4b8a32b1f41cda2846d2fa2ff2d40e12f14baa
Miguel Garza Carranza <maiky@meta.com>
2023-07-29 02:12:44 -0700

    Added darkMode on individual courses

--- ans2ask-ui/src/components/Course/Course.jsx
+++ ans2ask-ui/src/components/Course/Course.jsx
@@ -1,39 +1,42 @@
-import React from "react";
+import React, { useState, useContext } from 'react';
+import { UserContext } from '../../UserContext.js';
 import "./Course.css";
 
 const Course = ({video}) => {
-  return (
-    <div className="Course">
-        <div className="card mt-4" style={{ width: '900px' }}>
-            <div className="row no-gutters">
-            <div className="col-md-6">
-                <iframe
-                width="100%"
-                height="315"
-                src={`https://www.youtube.com/embed/${video.videoDetails.id.videoId}`}
-                title="YouTube Video Player"
-                frameBorder="0"
-                allowFullScreen
-                />
-            </div>
-            <div className="col-md-6">
-                <div className="card-body">
-                    <h5 className="card-title">{video.title}</h5>
-                    <p className="card-text">
-                        {video.views} | {video.date} | Duration: {video.duration} | {video.likes} likes
-                    </p>
-                    <div className="d-flex flex-column align-items-center" style={{marginTop: "30px"}}>
-                        <div style={{ maxWidth: '100px' }}>
-                            <img src={video.channel.thumbnail} className="card-img-top" alt={video.channel.name} />
+    const { darkMode } = useContext(UserContext);
+
+    return (
+        <div className="Course">
+            <div className="card border mt-4" style={{ backgroundColor: darkMode ? "#2D3748" : "rgba(248,249,250,1)", width: '900px' }}>
+                <div className="row no-gutters">
+                <div className="col-md-6">
+                    <iframe
+                    className='border'
+                    width="100%"
+                    height="315"
+                    src={`https://www.youtube.com/embed/${video.videoDetails.id.videoId}`}
+                    title="YouTube Video Player"
+                    frameBorder="0"
+                    allowFullScreen
+                    />
+                </div>
+                <div className="col-md-6">
+                    <div className="card-body">
+                        <h5 className="card-title" style={{color: darkMode ? "rgba(255, 255, 255, 0.92)" : "rgba(0,0,0,1)"}}>{video.title}</h5>
+                        <p className="card-text" style={{color: darkMode ? "rgba(255, 255, 255, 0.92)" : "rgba(0,0,0,1)"}}>
+                            {video.views} | {video.date} | Duration: {video.duration} | {video.likes} likes
+                        </p>
+                        <div className="d-flex flex-column align-items-center" style={{marginTop: "30px"}}>
+                            <div style={{ maxWidth: '100px' }}>
+                                <img src={video.channel.thumbnail} className="card-img-top" alt={video.channel.name} />
+                            </div>
+                            <a href={video.channel.link} className="btn btn-info mt-2 fw-bold">Visit Channel</a>
                         </div>
-                        <a href={video.channel.link} className="btn btn-info mt-2">Visit Channel</a>
                     </div>
                 </div>
-            </div>
+                </div>
             </div>
         </div>
-    </div>
-
   );
 }
 
