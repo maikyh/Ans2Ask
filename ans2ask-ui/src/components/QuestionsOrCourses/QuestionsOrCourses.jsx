95ec924010e83aea5dd857b6e267ce19481e1985
Miguel Garza Carranza <maiky@meta.com>
2023-07-28 22:06:30 -0700

    Created darkmode on QuestionOrCourses buttons

--- ans2ask-ui/src/components/QuestionsOrCourses/QuestionsOrCourses.jsx
+++ ans2ask-ui/src/components/QuestionsOrCourses/QuestionsOrCourses.jsx
@@ -1,22 +1,25 @@
-import React from "react";
+import React, { useContext } from 'react';
 import Options from "../../utils/OptionsQC.jsx";
-import Status from "../../utils/StatusQC.jsx";
+import { UserContext } from "../../UserContext.js";
+import { StatusL, StatusD } from "../../utils/StatusQC.jsx";
 import "./QuestionsOrCourses.css";
 
 const QuestionsOrCourses = ({selectedOption, handleSetSelectedOption}) => {
+  const { darkMode } = useContext(UserContext);
+
   return (
     <div className="QuestionsOrCourses">
       <div className="d-flex">
         <button
           onClick={() => handleSetSelectedOption(1)}
-          className={selectedOption === Options.question ? Status.activeQuestions : Status.inactiveQuestions}
+          className={ !darkMode ? selectedOption === Options.question ? StatusL.activeQuestions : StatusL.inactiveQuestions : selectedOption === Options.question ? StatusD.activeQuestions : StatusD.inactiveQuestions}
           data-tooltip="MOST RECENT QUESTIONS"
         >
           <h4>Questions</h4>
         </button>
         <button
           onClick={() => handleSetSelectedOption(2)}
-          className={selectedOption === Options.course ? Status.activeCourses : Status.inactiveCourses}
+          className={ !darkMode ? selectedOption === Options.course ? StatusL.activeCourses : StatusL.inactiveCourses : selectedOption === Options.course ? StatusD.activeCourses : StatusD.inactiveCourses}
           data-tooltip="BEST YOUTUBE COURSES (Google/Youtube Scraping)"
         >
           <h4>Courses</h4>
--- ans2ask-ui/src/utils/StatusQC.jsx
+++ ans2ask-ui/src/utils/StatusQC.jsx
@@ -1,8 +1,15 @@
-const Status = {
+const StatusL = {
     activeQuestions: "btn btn-dark w-100 d-block fw-bold mx-4 mt-0 mb-1 p-2",
     inactiveQuestions: "btn btn-outline-dark w-100 d-block fw-bold mx-4 mt-0 mb-1 p-2",
     activeCourses: "btn btn-dark w-100 d-block fw-bold mx-4 mt-0 mb-1 p-2",
     inactiveCourses: "btn btn-outline-dark w-100 d-block fw-bold mx-4 mt-0 mb-1 p-2"    
 }
 
-export default Status;
+const StatusD = {
+    activeQuestions: "btn btn-light w-100 d-block fw-bold mx-4 mt-0 mb-1 p-2",
+    inactiveQuestions: "btn btn-outline-light w-100 d-block fw-bold mx-4 mt-0 mb-1 p-2",
+    activeCourses: "btn btn-light w-100 d-block fw-bold mx-4 mt-0 mb-1 p-2",
+    inactiveCourses: "btn btn-outline-light w-100 d-block fw-bold mx-4 mt-0 mb-1 p-2"    
+}
+
+export { StatusL, StatusD };
