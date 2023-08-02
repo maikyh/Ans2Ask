447977aa5361660e20d94dc4e161be80c8a45698
Miguel Garza Carranza <maiky@meta.com>
2023-08-01 17:37:10 -0700

    Updated user Agent for web scraping

--- ans2ask-api/routes/googleScraping.js
+++ ans2ask-api/routes/googleScraping.js
@@ -9,7 +9,7 @@ const getDataFromGoogleSearchResults = (query) => {
         .get(`https://www.google.com/search?q=${query}+youtube+courses&tbm=vid`)
         .headers({
             "User-Agent":
-                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15, Custom-Agent/1.0"
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36, Custom-Agent/1.0"
         })
         .then((response) => {
             const $ = cheerio.load(response.body);
