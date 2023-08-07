d8903905cd45c280401864bac0efdeb69748e602
Miguel Garza Carranza <maiky@meta.com>
2023-08-07 01:43:14 -0700

    Polished code of home.jsx

--- ans2ask-ui/src/components/Home/Home.jsx
+++ ans2ask-ui/src/components/Home/Home.jsx
@@ -14,9 +14,11 @@ const LazyQuestionsOrCourses = React.lazy(() => import('../QuestionsOrCourses/Qu
 const LazyQuestionGrid = React.lazy(() => import('../QuestionGrid/QuestionGrid'));
 
 const Home = ({ images, handleSetSearchQuery }) => {
-    const { user, updateUser, darkMode } = useContext(UserContext);
+    const { updateUser, darkMode } = useContext(UserContext);
     const [selectedSubject, setSelectedSubject] = useState("All");
     const [selectedOption, setSelectedOption] = useState(Options.question);
+
+    const navigate = useNavigate();
 
     const handleSetSelectedSubject = (subject) => {
         setSelectedSubject(subject);
@@ -25,8 +27,6 @@ const Home = ({ images, handleSetSearchQuery }) => {
     const handleSetSelectedOption = (option) => {
         setSelectedOption(option);
     };
-
-    const navigate = useNavigate();
 
     const handleLogout = () => {
         updateUser(null);
