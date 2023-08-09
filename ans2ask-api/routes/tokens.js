5c1287cc7bc60558ef76cc62e36c35c0721cdb7e
Miguel Garza Carranza <maiky@meta.com>
2023-08-08 21:42:38 -0700

    Created token routes (get, create and delete)

---
+++ ans2ask-api/routes/tokens.js
@@ -0,0 +1,56 @@
+import express from 'express';
+import { User } from '../models/index.js';
+import { Token } from '../models/token.js';
+
+const router = express.Router();
+
+// Route to get all tokens, with associated users
+router.get('/tokens', async (req, res) => {
+    try {
+        const tokens = await Token.findAll({
+            include: [{ model: User, as: 'user' }],
+            order: [['createdAt', 'DESC']]
+        });
+        res.json(tokens);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// Route to create a new token
+router.post('/tokens', async (req, res) => {
+    try {
+        // Check if user is logged in
+        if (!req.session.user) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+
+        // Retrieve the current user from the session
+        const currentUser = req.session.user;
+
+        // Create the question with the current user ID
+        const token = await Token.create({
+            ...req.body,
+            userId: currentUser.id
+        });
+
+        res.status(201).json(token);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// Route to delete a token
+router.delete('/tokens', async (req, res) => {
+    const { token } = req.body;
+
+    try {
+        const tokenToDelete = await Token.destroy({ where: { token } });
+        
+        res.status(201).json("Token deleted");
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+export default router;
--- ans2ask-api/server.js
+++ ans2ask-api/server.js
@@ -10,6 +10,7 @@ import googleScrapingRoutes from './routes/googleScraping.js';
 import youtubeScrapingRoutes from './routes/youtubeScraping.js';
 import cloudinaryRoutes from './routes/cloudinary.js';
 import sendEmailRoutes from './routes/sendEmail.js';
+import tokenRoutes from './routes/tokens.js';
 import SequelizeStoreInit from 'connect-session-sequelize';
 import dotenv from "dotenv";
 
@@ -53,6 +54,7 @@ app.use(googleScrapingRoutes);
 app.use(youtubeScrapingRoutes);
 app.use(cloudinaryRoutes);
 app.use(sendEmailRoutes);
+app.use(tokenRoutes);
 
 sequelize.sync({ alter: true })
     .then(() => {
